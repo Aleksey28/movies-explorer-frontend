d6d347017addc22251e9ae7a0b43107eb49a8676
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-18 00:41:30 +0400

    feat: changed styles for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,26 @@
-import './Header.css';
+import "./Header.css";
 import React from "react";
-import { Link } from "react-router-dom";
-import iconLogo from '../../images/logo.svg';
+import { Link, NavLink } from "react-router-dom";
+import iconLogo from "../../images/logo.svg";
 import Navigation from "../Navigation/Navigation";
-import ProfileMenu from "../ProfileMenu/ProfileMenu";
 
-function Header({loggedIn}) {
+function Header({ loggedIn }) {
   return (
     <header className="header">
-      <Link to={'/'}>
+      <Link className="header__logo" to={"/"}>
         <img src={iconLogo} alt="Логотип. Переход к главной странице."/>
       </Link>
-      {loggedIn && <Navigation />}
-      <ProfileMenu loggedIn={loggedIn}/>
+      {loggedIn && <Navigation/>}
+      {!loggedIn &&
+      <ul className="header__auth">
+        <li>
+          <NavLink className="header__btn header__btn_type_signup" to="/signup">Регистрация</NavLink>
+        </li>
+        <li>
+          <NavLink className="header__btn header__btn_type_signin" to="/signin">Войти</NavLink>
+        </li>
+      </ul>
+      }
     </header>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,28 @@
 import "./Navigation.css";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import ProfileIcon from "../../images/profile.svg";
 
 function Navigation() {
   return (
     <ul className="navigation">
-      <li><NavLink className="navigation__item" activeClassName="navigation__item_active" to={"/"}>Главная</NavLink>
+      <li className="navigation__item">
+        <NavLink className="navigation__link" activeClassName="navigation__link_active" to={"/"}>Главная</NavLink>
       </li>
-      <li><NavLink className="navigation__item"
-                   activeClassName="navigation__item_active"
-                   to={"/movies"}>Фильмы</NavLink></li>
-      <li><NavLink className="navigation__item"
-                   activeClassName="navigation__item_active"
-                   to={"/saved-movies"}>Сохраненные фильмы</NavLink></li>
+      <li className="navigation__item">
+        <NavLink className="navigation__link" activeClassName="navigation__link_active" to={"/movies"}>Фильмы</NavLink>
+      </li>
+      <li className="navigation__item">
+        <NavLink className="navigation__link"
+                 activeClassName="navigation__link_active"
+                 to={"/saved-movies"}>Сохраненные фильмы</NavLink>
+      </li>
+      <li className="navigation__item navigation__item_type_profile">
+        <NavLink className="navigation__link navigation__link_type_profile" to="/profile">
+          Аккаунт
+          <img src={ProfileIcon} alt="Переход к профилю."/>
+        </NavLink>
+      </li>
     </ul>
   );
 }
