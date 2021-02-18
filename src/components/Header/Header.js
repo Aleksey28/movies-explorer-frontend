65193bbe313e376566a9d46b0ddbb4cd0160708a
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-19 00:38:02 +0400

    feat: added styles fot public type header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,33 +12,35 @@ function Header({ loggedIn }) {
   };
   return (
     <header className={`header ${!loggedIn && "header_type_public"}`}>
-      <div className="header__top">
-        <Link className="header__logo" to={"/"}>
-          <img src={iconLogo} alt="Логотип. Переход к главной странице."/>
-        </Link>
-        {loggedIn
-         ? <>
-           <Navigation openNavigation={openNavigation}/>
-           <button className={`header__switch-menu ${openNavigation && "header__switch-menu_opened"}`}
-                   onClick={handleClickOnNavSwitch}/>
-         </>
-         : <ul className="header__auth">
-           <li>
-             <NavLink className="header__link header__link_type_signup" to="/signup">Регистрация</NavLink>
-           </li>
-           <li>
-             <NavLink className="header__link header__link_type_signin" to="/signin">Войти</NavLink>
-           </li>
-         </ul>
-        }
-      </div>
-      <div className="header__bottom">
-        <div className="header__description">
-          <h1 className="header__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
-          <p className="header__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-          <a href="" className="header__show-more">Узнать больше</a>
+      <div className="header__container">
+        <div className="header__top">
+          <Link className="header__logo" to={"/"}>
+            <img src={iconLogo} alt="Логотип. Переход к главной странице."/>
+          </Link>
+          {loggedIn
+           ? <>
+             <Navigation openNavigation={openNavigation}/>
+             <button className={`header__switch-menu ${openNavigation && "header__switch-menu_opened"}`}
+                     onClick={handleClickOnNavSwitch}/>
+           </>
+           : <ul className="header__auth">
+             <li>
+               <NavLink className="header__link header__link_type_signup" to="/signup">Регистрация</NavLink>
+             </li>
+             <li>
+               <NavLink className="header__link header__link_type_signin" to="/signin">Войти</NavLink>
+             </li>
+           </ul>
+          }
         </div>
-        <img src={imgEarth} alt="WEB around all of us." className="header__image"/>
+        <div className="header__bottom">
+          <div className="header__description">
+            <h1 className="header__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
+            <p className="header__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+            <a href="" className="header__show-more">Узнать больше</a>
+          </div>
+          <img src={imgEarth} alt="WEB around all of us." className="header__image"/>
+        </div>
       </div>
     </header>
   );
