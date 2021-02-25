51b30bc69388b85e33a8b7c14e902b880d6f3ce1
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-25 22:59:44 +0400

    feat: added component error

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,20 +2,28 @@ import "./App.css";
 import React, { useState } from "react";
 import Main from "../Main/Main";
 import Header from "../Header/Header";
-import { Route } from "react-router";
+import { Route, Switch } from "react-router";
 import Footer from "../Footer/Footer";
+import Error from "../Error/Error";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="page">
-      <div className={`page__container ${!loggedIn && "page__container_color_blue"}`}>
-        <Header loggedIn={loggedIn}/>
-      </div>
-      <Route exact path="/">
-        <Main/>
-      </Route>
-      <Footer/>
+      <Switch>
+        <Route path="/error">
+          <Error/>
+        </Route>
+        <Route path="/">
+          <div className={`page__container ${!loggedIn && "page__container_color_blue"}`}>
+            <Header loggedIn={loggedIn}/>
+          </div>
+          <Route exact path="/">
+            <Main/>
+            <Footer/>
+          </Route>
+        </Route>
+      </Switch>
     </div>
   );
 }
