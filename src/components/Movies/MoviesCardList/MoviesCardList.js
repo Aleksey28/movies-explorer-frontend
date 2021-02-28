acac21b5022081775a7e9a7c86f4299b4d94099b
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-03-01 01:20:36 +0400

    feat: changed MoviesCardList.js

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./MoviesCardList.css";
-import { cards } from "../../../utils/constants";
 import MoviesCard from "../MoviesCard/MoviesCard";
 
-function MoviesCardList() {
-  const cardElements = cards.map((item) => <li key={item.id}><MoviesCard data={item}/></li>);
+function MoviesCardList({ cards }) {
+  const cardElements = cards.slice(0,12).map((item) => <li key={item.id}><MoviesCard data={item}/></li>);
   return (
     <ul className="cards">
       {cardElements}
