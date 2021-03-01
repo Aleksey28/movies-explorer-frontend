c86913afd5affe3372c5de2b2749f227395c7132
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-03-02 02:43:03 +0400

    styled: adapted Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,18 +7,18 @@ function Navigation({ openNavigation }) {
   return (
     <div className={`navigation ${openNavigation && "navigation_opened"}`}>
       <ul className={`navigation__list ${openNavigation && "navigation__list_opened"}`}>
-        <li className="navigation__item">
+        <li className="navigation__item navigation__item_visible-in-bar">
           <NavLink className="navigation__link"
                    activeClassName="navigation__link_active"
                    exact
                    to={"/"}>Главная</NavLink>
         </li>
-        <li className="navigation__item">
+        <li>
           <NavLink className="navigation__link"
                    activeClassName="navigation__link_active"
                    to={"/movies"}>Фильмы</NavLink>
         </li>
-        <li className="navigation__item">
+        <li>
           <NavLink className="navigation__link"
                    activeClassName="navigation__link_active"
                    to={"/saved-movies"}>Сохраненные фильмы</NavLink>
