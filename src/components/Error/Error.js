e094ac9fd6c5c1d83217cfe4a4482cfefc8e88da
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-03-03 18:56:25 +0400

    feat: last changed

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Error.css";
+import { useHistory } from "react-router";
 
 function Error({ status = 404, message = "Страница не найдена" }) {
+
+  const history = useHistory();
+
   return (
     <div className="error">
       <div className="error__container">
         <p className="error__status">{status}</p>
         <p className="error__message">{message}</p>
       </div>
-      <button className="error__back">Назад</button>
+      <button className="error__back" onClick={history.goBack}>Назад</button>
     </div>
   );
 }
--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -3,7 +3,6 @@ import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
 
 function MoviesCardList({ cards }) {
-  console.log(window.screen.width);
   const countCards = window.screen.width > 768 ? 12 : window.screen.width > 400 ? 8 : 5;
   const cardElements = cards.slice(0,countCards).map((item) => <li key={item.id}><MoviesCard data={item}/></li>);
   return (
