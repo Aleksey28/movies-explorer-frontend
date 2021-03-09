b9e708f1a0181bc9b6ca2d601a35bd11704712a4
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-03-10 01:32:24 +0400

    feat: added main interface for api

---
+++ src/utils/Api.js
@@ -0,0 +1,37 @@
+export default class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _getProxy({ relativePath, method, body = "", headers = {} }) {
+    const options = {
+      method,
+      // credential: "include",
+      headers: { ...this._headers, ...headers },
+    };
+
+    if (body) {
+      options.body = body;
+    }
+
+    return fetch(`${this._baseUrl}${relativePath}`, options);
+  }
+
+  async _handleResponse(response) {
+    const description = await response.json();
+
+    if (response.ok) {
+      return description;
+    } else {
+      return Promise.reject({
+        status: response.status,
+        message: description.message
+                 ? description.message
+                 : description.error
+                   ? description.error
+                   : "Что-то пошло не так!\nПопробуйте ещё раз.",
+      });
+    }
+  }
+}
