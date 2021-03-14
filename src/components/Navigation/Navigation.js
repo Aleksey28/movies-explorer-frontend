ede8aedfe5ae9445ec70e4ae642cb347bda36fc7
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-03-14 17:21:46 +0400

    feat: restyled header and added possibility closing navigation with click on overlay

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,22 +3,32 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import ProfileIcon from "../../images/profile.svg";
 
-function Navigation({ openNavigation }) {
+function Navigation({ openNavigation, onCloseNavigation }) {
+  const handleClickOverlay = (e) => {
+    if (e.target === e.currentTarget) {
+      onCloseNavigation();
+    }
+  };
   return (
-    <div className={`navigation ${openNavigation && "navigation_opened"}`}>
-      <ul className={`navigation__list ${openNavigation && "navigation__list_opened"}`}>
-        <li className="navigation__item navigation__item_visible-in-bar">
+    <div
+      className={`navigation ${openNavigation && "navigation_opened"}`}
+      onClick={handleClickOverlay}>
+      <ul
+        className={`navigation__list ${openNavigation && "navigation__list_opened"}`}>
+        <li
+          className="navigation__item navigation__item_visible-in-bar"
+          onClick={onCloseNavigation}>
           <NavLink className="navigation__link"
                    activeClassName="navigation__link_active"
                    exact
                    to={"/"}>Главная</NavLink>
         </li>
-        <li>
+        <li onClick={onCloseNavigation}>
           <NavLink className="navigation__link"
                    activeClassName="navigation__link_active"
                    to={"/movies"}>Фильмы</NavLink>
         </li>
-        <li>
+        <li onClick={onCloseNavigation}>
           <NavLink className="navigation__link"
                    activeClassName="navigation__link_active"
                    to={"/saved-movies"}>Сохраненные фильмы</NavLink>
